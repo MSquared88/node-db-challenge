11fd049f01f2518d145f1d0d5317b9e434f7cc80
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-10-11 10:57:03 -0500

    setup file structure

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const server = express()
+
+const projectsRouter = require('./projects/projects-router')
+const resourcesRouter = require('./resources/resources-router')
+const tasksRouter = require('./tasks/tasks-router')
+
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send('server is running')
+})
+
+// server.use('/api/projects', projectsRouter)
+// server.use('/api/resources', resourcesRouter)
+// server.use('/api/tasks', tasksRouter)
+
+
+module.exports = server
